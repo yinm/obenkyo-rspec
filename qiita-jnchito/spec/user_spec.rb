a44f79802964bc983e34348c0c0beb1fae08f3cb
yinm <yinm.cminor9@gmail.com>
2017-11-12 14:47:51 +0900

    Mod it block

--- qiita-jnchito/spec/user_spec.rb
+++ qiita-jnchito/spec/user_spec.rb
@@ -6,15 +6,11 @@ describe User do
     subject { user.greet }
     context '12歳以下の場合' do
       let(:age) { 12 }
-      it 'ひらがなで答えること' do
-        is_expected.to eq 'ぼくはたろうだよ。'
-      end
+      it { is_expected.to eq 'ぼくはたろうだよ。' }
     end
-    context '13歳以下の場合' do
+    context '13歳以上の場合' do
       let(:age) { 13 }
-      it '漢字で答えること' do
-        is_expected.to eq '僕はたろうです。'
-      end
+      it { is_expected.to eq '僕はたろうです。' }
     end
   end
 end
