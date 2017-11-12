d12d91bca598f252a89e22a0650333fd5a3b04ce
yinm <yinm.cminor9@gmail.com>
2017-11-12 12:17:38 +0900

    Practice repeatedly

--- qiita-jnchito/spec/user_spec.rb
+++ qiita-jnchito/spec/user_spec.rb
@@ -1,20 +1,25 @@
 describe User do
   describe '#greet' do
-    let(:user) { User.new(params) }
-    let(:params) { { name: 'たろう', age: age } }
+    let(:params) { { name: 'たろう' } }
 
     context '12歳以下の場合' do
-      let(:age) { 12 }
+      before do
+        params.merge!(age: 12)
+      end
 
       it 'ひらがなで答えること' do
+        user = User.new(params)
         expect(user.greet).to eq 'ぼくはたろうだよ。'
       end
     end
 
     context '13歳以上の場合' do
-      let(:age) { 13 }
+      before do
+        params.merge!(age: 13)
+      end
 
       it '漢字で答えること' do
+        user = User.new(params)
         expect(user.greet).to eq '僕はたろうです。'
       end
     end
