55afd24e55997e0a97fa7acea62cbdd3fa25bc8d
yinm <yinm.cminor9@gmail.com>
2017-11-12 12:53:10 +0900

    Add shared_contexts

--- qiita-jnchito/spec/user_spec.rb
+++ qiita-jnchito/spec/user_spec.rb
@@ -1,31 +1,38 @@
 describe User do
+  let(:user) { User.new(name: 'たろう', age: age) }
+
+  shared_context '12歳の場合' do
+    let(:age) { 12 }
+  end
+  shared_context '13歳の場合' do
+    let(:age) { 13 }
+  end
+
   describe '#greet' do
-    let(:user) { User.new(name: 'たろう', age: age) }
     subject { user.greet }
 
     context '12歳以下の場合' do
-      let(:age) { 12 }
+      include_context '12歳の場合'
       it { is_expected.to eq 'ぼくはたろうだよ。' }
     end
 
     context '13歳以上の場合' do
-      let(:age) { 13 }
+      include_context '13歳の場合'
       it { is_expected.to eq '僕はたろうです。' }
     end
 
   end
 
   describe '#child?' do
-    let(:user) { User.new(name: 'たろう', age: age) }
     subject { user.child? }
 
     context '12歳以下の場合' do
-      let(:age) { 12 }
+      include_context '12歳の場合'
       it { is_expected.to eq true }
     end
 
     context '13歳以上の場合' do
-      let(:age) { 13 }
+      include_context '13歳の場合'
       it { is_expected.to eq false }
     end
 
