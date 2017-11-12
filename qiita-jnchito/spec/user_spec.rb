63157a60b40db209a63e5a3175e6c277446bd1e6
yinm <yinm.cminor9@gmail.com>
2017-11-12 14:41:39 +0900

    Mod let

--- qiita-jnchito/spec/user_spec.rb
+++ qiita-jnchito/spec/user_spec.rb
@@ -1,23 +1,17 @@
 describe User do
   describe '#greet' do
     let(:user) { User.new(params) }
-    let(:params) { { name: 'たろう' } }
+    let(:params) { { name: 'たろう', age: age } }
 
     context '12歳以下の場合' do
-      before do
-        params.merge!(age: 12)
-      end
-
+      let(:age) { 12 }
       it 'ひらがなで答えること' do
         expect(user.greet).to eq 'ぼくはたろうだよ。'
       end
     end
 
-    context '13歳以下の場合' do
-      before do
-        params.merge!(age: 13)
-      end
-
+    context '13歳以上の場合' do
+      let(:age) { 13 }
       it '漢字で答えること' do
         expect(user.greet).to eq '僕はたろうです。'
       end
