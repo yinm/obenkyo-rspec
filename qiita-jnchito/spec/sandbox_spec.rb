afaf074024f330961760de9b79629d7b2400d6f9
yinm <yinm.cminor9@gmail.com>
2017-11-13 08:45:31 +0900

    Add eq matcher

--- qiita-jnchito/spec/sandbox_spec.rb
+++ qiita-jnchito/spec/sandbox_spec.rb
@@ -2,6 +2,6 @@ describe 'matcher' do
   it '動作確認' do
     message_1 = 'Hello'
     message_2 = 'Hello'
-    expect([message_1].first).not_to be message_2
+    expect([message_1].first).to eq message_2
   end
 end
