cba1fb1f879b79c5ec0611cd4b0bb8c87d482291
yinm <yinm.cminor9@gmail.com>
2017-11-12 12:39:29 +0900

    Add shared_examples

--- qiita-jnchito/spec/user_spec.rb
+++ qiita-jnchito/spec/user_spec.rb
@@ -3,24 +3,30 @@ describe User do
     let(:user) { User.new(name: 'たろう', age: age) }
     subject { user.greet }
 
-    context '0歳の場合' do
-      let(:age) { 0 }
+    shared_examples '子どものあいさつ' do
       it { is_expected.to eq 'ぼくはたろうだよ。' }
     end
 
+    context '0歳の場合' do
+      let(:age) { 0 }
+      it_behaves_like '子どものあいさつ'
+    end
     context '12歳の場合' do
       let(:age) { 12 }
-      it { is_expected.to eq 'ぼくはたろうだよ。' }
+      it_behaves_like '子どものあいさつ'
+    end
+
+    shared_examples '大人のあいさつ' do
+      it { is_expected.to eq '僕はたろうです。' }
     end
 
     context '13歳の場合' do
       let(:age) { 13 }
-      it { is_expected.to eq '僕はたろうです。' }
+      it_behaves_like '大人のあいさつ'
     end
-
     context '100歳の場合' do
       let(:age) { 100 }
-      it { is_expected.to eq '僕はたろうです。' }
+      it_behaves_like '大人のあいさつ'
     end
 
   end
