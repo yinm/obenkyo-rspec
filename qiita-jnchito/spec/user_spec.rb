baaab82a68e6e5c6a135aaf02efbd92c21402ac4
yinm <yinm.cminor9@gmail.com>
2017-11-12 14:24:39 +0900

    Add before block

--- qiita-jnchito/spec/user_spec.rb
+++ qiita-jnchito/spec/user_spec.rb
@@ -1,17 +1,22 @@
 describe User do
   describe '#greet' do
+    before do
+      @params = { name: 'たろう' }
+    end
+
     context '12歳以下の場合' do
       it 'ひらがなで答えること' do
-        user = User.new(name: 'たろう', age: 12)
+        user = User.new(@params.merge(age: 12))
         expect(user.greet).to eq 'ぼくはたろうだよ。'
       end
     end
 
     context '13歳以上の場合' do
       it '漢字で答えること' do
-        user = User.new(name: 'たろう', age: 13)
+        user = User.new(@params.merge(age: 13))
         expect(user.greet).to eq '僕はたろうです。'
       end
     end
+
   end
 end
