7766b8980ab6d221feeca547f3379f1bbd77abdc
yinm <yinm.cminor9@gmail.com>
2017-11-12 13:09:43 +0900

    Add todo tests

--- qiita-jnchito/spec/user_spec.rb
+++ qiita-jnchito/spec/user_spec.rb
@@ -1,41 +1,11 @@
 describe User do
-  let(:user) { User.new(name: 'たろう', age: age) }
-
-  shared_context '12歳の場合' do
-    let(:age) { 12 }
-  end
-  shared_context '13歳の場合' do
-    let(:age) { 13 }
-  end
-
-  describe '#greet' do
-    subject { user.greet }
-
+  describe '#good_bye' do
     context '12歳以下の場合' do
-      include_context '12歳の場合'
-      it { is_expected.to eq 'ぼくはたろうだよ。' }
+      it 'ひらがなでさよならすること'
     end
 
     context '13歳以上の場合' do
-      include_context '13歳の場合'
-      it { is_expected.to eq '僕はたろうです。' }
+      it '漢字でさよならすること'
     end
-
   end
-
-  describe '#child?' do
-    subject { user.child? }
-
-    context '12歳以下の場合' do
-      include_context '12歳の場合'
-      it { is_expected.to eq true }
-    end
-
-    context '13歳以上の場合' do
-      include_context '13歳の場合'
-      it { is_expected.to eq false }
-    end
-
-  end
-
 end
