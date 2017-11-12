878c7b998a541e03618c7544c9f27cfd7ead52c9
yinm <yinm.cminor9@gmail.com>
2017-11-12 12:06:43 +0900

    change from before to let

--- qiita-jnchito/spec/user_spec.rb
+++ qiita-jnchito/spec/user_spec.rb
@@ -1,27 +1,25 @@
 describe User do
   describe '#greet' do
-    before do
-      @params = { name: 'たろう' }
-    end
+    let(:params) { { name: 'たろう' } }
 
     context '12歳以下の場合' do
       before do
-        @params.merge!(age: 12)
+        params.merge!(age: 12)
       end
 
       it 'ひらがなで答えること' do
-        user = User.new(@params)
+        user = User.new(params)
         expect(user.greet).to eq 'ぼくはたろうだよ。'
       end
     end
 
     context '13歳以上の場合' do
       before do
-        @params.merge!(age: 13)
+        params.merge!(age: 13)
       end
 
       it '漢字で答えること' do
-        user = User.new(@params)
+        user = User.new(params)
         expect(user.greet).to eq '僕はたろうです。'
       end
     end
