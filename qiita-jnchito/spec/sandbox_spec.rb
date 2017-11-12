84eeff8f1674009ef6e9e73b16f1d768b9359e5a
yinm <yinm.cminor9@gmail.com>
2017-11-12 14:15:12 +0900

    Practice repeatedly

--- qiita-jnchito/spec/sandbox_spec.rb
+++ qiita-jnchito/spec/sandbox_spec.rb
@@ -1,8 +1,13 @@
 describe '四則演算' do
-  it '全部できること' do
-    expect(1 + 2).to eq 3
-    expect(10 - 1).to eq 9
-    expect(4 * 8).to eq 32
-    expect(40 / 5).to eq 8
+  describe '足し算' do
+    it '1 + 1 は 2 になること' do
+      expect(1 + 1).to eq 2
+    end
+  end
+
+  describe '引き算' do
+    it '10 - 1 は 9 になること' do
+      expect(10 - 1).to eq 9
+    end
   end
 end
