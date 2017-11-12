8dc92c23823e23b0e36e88d94a727eb2ddbdd9da
yinm <yinm.cminor9@gmail.com>
2017-11-12 12:30:01 +0900

    Add subject

--- qiita-jnchito/spec/user_spec.rb
+++ qiita-jnchito/spec/user_spec.rb
@@ -3,11 +3,13 @@ describe User do
     let(:user) { User.new(params) }
     let(:params) { { name: 'たろう', age: age } }
 
+    subject { user.greet }
+
     context '12歳以下の場合' do
       let(:age) { 12 }
 
       it 'ひらがなで答えること' do
-        expect(user.greet).to eq 'ぼくはたろうだよ。'
+        is_expected.to eq 'ぼくはたろうだよ。'
       end
     end
 
@@ -15,7 +17,7 @@ describe User do
       let(:age) { 13 }
 
       it '漢字で答えること' do
-        expect(user.greet).to eq '僕はたろうです。'
+        is_expected.to eq '僕はたろうです。'
       end
     end
 
