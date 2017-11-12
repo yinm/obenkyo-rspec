9db27f256af1a4bbe76c00d3403d7d901c772669
yinm <yinm.cminor9@gmail.com>
2017-11-12 11:48:07 +0900

    Add context for easily reading

--- qiita-jnchito/spec/user_spec.rb
+++ qiita-jnchito/spec/user_spec.rb
@@ -1,13 +1,17 @@
 describe User do
   describe '#greet' do
-    it '12歳以下の場合、ひらがなで答えること' do
-      user = User.new(name: 'たろう', age: 12)
-      expect(user.greet).to eq 'ぼくはたろうだよ。'
+    context '12歳以下の場合' do
+      it 'ひらがなで答えること' do
+        user = User.new(name: 'たろう', age: 12)
+        expect(user.greet).to eq 'ぼくはたろうだよ。'
+      end
     end
 
-    it '13歳以上の場合、漢字で答えること' do
-      user = User.new(name: 'たろう', age: 13)
-      expect(user.greet).to eq '僕はたろうです。'
+    context '13歳以上の場合' do
+      it '漢字で答えること' do
+        user = User.new(name: 'たろう', age: 13)
+        expect(user.greet).to eq '僕はたろうです。'
+      end
     end
   end
 end
