fc8280f0850e9955ec8c5df1012c572ac96a270d
yinm <yinm.cminor9@gmail.com>
2017-11-12 13:01:30 +0900

    Add xdescribe and xcontext

--- qiita-jnchito/spec/sandbox_spec.rb
+++ qiita-jnchito/spec/sandbox_spec.rb
@@ -1,7 +1,19 @@
-describe '何らかの理由で実行したくないクラス' do
-  xit '実行したくないテスト' do
-    expect(1 + 2).to eq 3
+xdescribe '四則演算' do
+  it '1 + 1 は 2 になること' do
+    expect(1 + 1).to eq 2
+  end
 
-    expect(foo).to eq bar
+  it '10 - 1 は 9 になること' do
+    expect(10 -1).to eq 9
+  end
+end
+
+xcontext '管理者の場合' do
+  it '社員情報を編集できる' do
+
+  end
+
+  it '社員情報を削除できる' do
+
   end
 end
