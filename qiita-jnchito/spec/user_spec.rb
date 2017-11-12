2e28fd726d17cb8bfaed1437ed99c9279b10c414
yinm <yinm.cminor9@gmail.com>
2017-11-12 11:54:23 +0900

    Add nested before block

--- qiita-jnchito/spec/user_spec.rb
+++ qiita-jnchito/spec/user_spec.rb
@@ -5,17 +5,26 @@ describe User do
     end
 
     context '12歳以下の場合' do
+      before do
+        @params.merge!(age: 12)
+      end
+
       it 'ひらがなで答えること' do
-        user = User.new(@params.merge(age: 12))
+        user = User.new(@params)
         expect(user.greet).to eq 'ぼくはたろうだよ。'
       end
     end
 
     context '13歳以上の場合' do
+      before do
+        @params.merge!(age: 13)
+      end
+
       it '漢字で答えること' do
-        user = User.new(@params.merge(age: 13))
+        user = User.new(@params)
         expect(user.greet).to eq '僕はたろうです。'
       end
     end
+
   end
 end
