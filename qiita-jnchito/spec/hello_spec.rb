71207fa2494d7f7ca89a6f3a0ba4387d8e67f6f5
yinm <yinm.cminor9@gmail.com>
2017-11-12 14:11:10 +0900

    Practice repeatedly

--- qiita-jnchito/spec/hello_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-
-RSpec.describe Hello do
-  it 'message return hello' do
-    expect(Hello.new.message).to eq 'hello'
-  end
-end
--- qiita-jnchito/spec/sandbox_spec.rb
+++ qiita-jnchito/spec/sandbox_spec.rb
@@ -1,19 +1,5 @@
-xdescribe '四則演算' do
+describe '四則演算' do
   it '1 + 1 は 2 になること' do
     expect(1 + 1).to eq 2
-  end
-
-  it '10 - 1 は 9 になること' do
-    expect(10 -1).to eq 9
-  end
-end
-
-xcontext '管理者の場合' do
-  it '社員情報を編集できる' do
-
-  end
-
-  it '社員情報を削除できる' do
-
   end
 end
