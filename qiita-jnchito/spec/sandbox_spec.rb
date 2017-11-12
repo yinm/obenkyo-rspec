3bae45efc75908c5789bf830d2db1ae499171980
yinm <yinm.cminor9@gmail.com>
2017-11-13 08:47:06 +0900

    Add be and eq matcher

--- qiita-jnchito/spec/sandbox_spec.rb
+++ qiita-jnchito/spec/sandbox_spec.rb
@@ -1,7 +1,15 @@
 describe 'matcher' do
   it '動作確認' do
-    message_1 = 'Hello'
-    message_2 = 'Hello'
-    expect([message_1].first).to eq message_2
+    expect(true).to be true
+    expect(false).to be false
+    expect(nil).to be nil
+    expect(1 + 1).to be 2
+    expect(:foo).to be :foo
+
+    expect(true).to eq true
+    expect(false).to eq false
+    expect(nil).to eq nil
+    expect(1 + 1).to eq 2
+    expect(:foo).to eq :foo
   end
 end
