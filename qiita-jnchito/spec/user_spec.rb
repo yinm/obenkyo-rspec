d336a5f4da142ce18ec9ef020274d42c112ec422
yinm <yinm.cminor9@gmail.com>
2017-11-12 16:09:36 +0900

    Add practice pattern

--- qiita-jnchito/spec/user_spec.rb
+++ qiita-jnchito/spec/user_spec.rb
@@ -1,15 +1,27 @@
 describe User do
   describe '#greet' do
     let(:user) { User.new(name: 'たろう', age: age) }
-
     subject { user.greet }
-    context '12歳以下の場合' do
+
+    context '0歳の場合' do
+      let(:age) { 0 }
+      it { is_expected.to eq 'ぼくはたろうだよ。' }
+    end
+
+    context '12歳の場合' do
       let(:age) { 12 }
       it { is_expected.to eq 'ぼくはたろうだよ。' }
     end
-    context '13歳以上の場合' do
+
+    context '13歳の場合' do
       let(:age) { 13 }
       it { is_expected.to eq '僕はたろうです。' }
     end
+
+    context '100歳の場合' do
+      let(:age) { 100 }
+      it { is_expected.to eq '僕はたろうです。' }
+    end
+
   end
 end
