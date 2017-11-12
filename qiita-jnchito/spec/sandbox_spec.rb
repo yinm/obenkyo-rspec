b2af7303d74f372122427efa5b140d5ae9d843f5
yinm <yinm.cminor9@gmail.com>
2017-11-13 08:48:45 +0900

    Add be_empty matcher

--- qiita-jnchito/spec/sandbox_spec.rb
+++ qiita-jnchito/spec/sandbox_spec.rb
@@ -1,15 +1,8 @@
 describe 'matcher' do
   it '動作確認' do
-    expect(true).to be true
-    expect(false).to be false
-    expect(nil).to be nil
-    expect(1 + 1).to be 2
-    expect(:foo).to be :foo
+    expect([]).to be_empty
 
-    expect(true).to eq true
-    expect(false).to eq false
-    expect(nil).to eq nil
-    expect(1 + 1).to eq 2
-    expect(:foo).to eq :foo
+    expect([].empty?).to be true
+    expect([].empty?).to eq true
   end
 end
