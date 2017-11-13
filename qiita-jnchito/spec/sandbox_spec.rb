c2a2f191c98bd63b059f54a6f544dd48722ca45f
yinm <yinm.cminor9@gmail.com>
2017-11-14 07:50:24 +0900

    Add examples

--- qiita-jnchito/spec/sandbox_spec.rb
+++ qiita-jnchito/spec/sandbox_spec.rb
@@ -1,22 +1,9 @@
 describe 'matcher' do
   it '動作確認' do
-    expect(1).to be_truthy
-    expect(nil).to be_falsey
-  end
+    x = [1, 2, 3]
+    expect(x.size).to eq 3
 
-  it '失敗1' do
-    expect(1).to be true
-  end
-
-  it '失敗2' do
-    expect(nil).to be false
-  end
-
-  it '失敗3' do
-    expect(1).to eq true
-  end
-
-  it '失敗4' do
-    expect(nil).to eq false
+    x.pop
+    expect(x.size).to eq 2
   end
 end
