b13069827c5ae9f28f3da3502ba07908faf7616a
yinm <yinm.cminor9@gmail.com>
2017-11-14 07:48:06 +0900

    Add be_truthy and be_falsey

--- qiita-jnchito/spec/sandbox_spec.rb
+++ qiita-jnchito/spec/sandbox_spec.rb
@@ -1,8 +1,22 @@
 describe 'matcher' do
   it '動作確認' do
-    expect([]).to be_empty
+    expect(1).to be_truthy
+    expect(nil).to be_falsey
+  end
 
-    expect([].empty?).to be true
-    expect([].empty?).to eq true
+  it '失敗1' do
+    expect(1).to be true
+  end
+
+  it '失敗2' do
+    expect(nil).to be false
+  end
+
+  it '失敗3' do
+    expect(1).to eq true
+  end
+
+  it '失敗4' do
+    expect(nil).to eq false
   end
 end
