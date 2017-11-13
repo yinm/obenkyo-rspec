4a7df4e10884b1064e14fdc14e0b12ffe9cd54d3
yinm <yinm.cminor9@gmail.com>
2017-11-14 08:10:37 +0900

    Add include

--- qiita-jnchito/spec/sandbox_spec.rb
+++ qiita-jnchito/spec/sandbox_spec.rb
@@ -1,6 +1,7 @@
 describe 'matcher' do
   it '動作確認' do
     x = [1, 2, 3]
-    expect{ x.push(10) }.to change{ x.size }.from(3).to(4)
+    expect(x).to include 1
+    expect(x).to include 1, 3
   end
 end
