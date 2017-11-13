9f1924872b1c565ef81c626ebac081d5c00e4984
yinm <yinm.cminor9@gmail.com>
2017-11-14 08:42:09 +0900

    Add mock test

---
+++ qiita-jnchito/spec/weather_bot_spec.rb
@@ -0,0 +1,13 @@
+describe WeatherBot do
+  describe '#tweet_forecast' do
+    it 'エラーなく予報をツイートすること' do
+      twitter_client_mock = double('Twitter client')
+      allow(twitter_client_mock).to receive(:update)
+
+      weather_bot = WeatherBot.new
+      allow(weather_bot).to receive(:twitter_client).and_return(twitter_client_mock)
+
+      expect{ weather_bot.tweet_forecast }.not_to raise_error
+    end
+  end
+end
