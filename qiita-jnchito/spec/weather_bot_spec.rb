d5d7e017054815bdfcf30bec360177f4d5ee030e
yinm <yinm.cminor9@gmail.com>
2017-11-15 08:05:36 +0900

    Add raising error test

--- qiita-jnchito/spec/weather_bot_spec.rb
+++ qiita-jnchito/spec/weather_bot_spec.rb
@@ -9,5 +9,16 @@ describe WeatherBot do
 
       expect{ weather_bot.tweet_forecast }.not_to raise_error
     end
+
+    it 'エラーが起きたら通知すること' do
+      twitter_client_mock = double('Twitter client')
+      allow(twitter_client_mock).to receive(:update).and_raise('エラーが発生しました')
+
+      weather_bot = WeatherBot.new
+      allow(weather_bot).to receive(:twitter_client).and_return(twitter_client_mock)
+      expect(weather_bot).to receive(:notify)
+
+      weather_bot.tweet_forecast
+    end
   end
 end
