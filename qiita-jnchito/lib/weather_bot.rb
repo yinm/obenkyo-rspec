561963450632e002ce2a9a6cd7fe6db38c83fe50
yinm <yinm.cminor9@gmail.com>
2017-11-15 08:06:27 +0900

    Add error handling

--- qiita-jnchito/lib/weather_bot.rb
+++ qiita-jnchito/lib/weather_bot.rb
@@ -3,9 +3,14 @@ require 'twitter'
 class WeatherBot
   def tweet_forecast
     twitter_client.update '今日は晴れです'
+  rescue => e
+    notify(e)
   end
 
   def twitter_client
     Twitter::REST::Client.new
+  end
+
+  def notify(error)
   end
 end
