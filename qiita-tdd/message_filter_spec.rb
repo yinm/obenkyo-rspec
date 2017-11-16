2edd0a93bc2d0fe17b38a8547a5e57e389c5371b
yinm <yinm.cminor9@gmail.com>
2017-11-17 08:08:36 +0900

    Use subject

--- qiita-tdd/message_filter_spec.rb
+++ qiita-tdd/message_filter_spec.rb
@@ -4,8 +4,8 @@ describe MessageFilter, 'with argument "foo"' do
   before do
     @filter = MessageFilter.new('foo')
   end
+  subject { @filter }
 
-  it { expect(@filter).to be_detect('hello from foo') }
-
-  it { expect(@filter).not_to be_detect('hello, world') }
+  it { is_expected.to be_detect('hello from foo') }
+  it { is_expected.not_to be_detect('hello, world') }
 end
