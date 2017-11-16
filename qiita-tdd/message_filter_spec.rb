9143f5e0657fec9214e5432fced8b58312a3be0b
yinm <yinm.cminor9@gmail.com>
2017-11-17 08:04:18 +0900

    Omit spec descriptions

--- qiita-tdd/message_filter_spec.rb
+++ qiita-tdd/message_filter_spec.rb
@@ -5,11 +5,7 @@ describe MessageFilter do
     @filter = MessageFilter.new('foo')
   end
 
-  it 'detects message with NG word' do
-    expect(@filter).to be_detect('hello from foo')
-  end
+  it { expect(@filter).to be_detect('hello from foo') }
 
-  it 'does not detect message without NG word' do
-    expect(@filter).not_to be_detect('hello, world')
-  end
+  it { expect(@filter).not_to be_detect('hello, world') }
 end
